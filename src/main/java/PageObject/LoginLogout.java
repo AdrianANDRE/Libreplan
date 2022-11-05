ca7a4797cb1c9b91c0a53a116e1ea10142a463f5
AdrianANDRE <andre.adrian11@gmail.com>
2022-11-05 21:49:50 +0100

    Toujours le weekend / fix le code

--- src/main/java/PageObject/LoginLogout.java
+++ src/main/java/PageObject/LoginLogout.java
@@ -3,21 +3,25 @@ package PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import static org.junit.Assert.assertTrue;
 
 public class LoginLogout extends ToolBox {
-    //Xpath field username
-    WebElement userField = driver.findElement(By.xpath("//input[@id=\"textfield\"]"));
-    //Xpath field password
-    WebElement passwordField = driver.findElement(By.xpath("//input[@id=\"textfield2\"]"));
-    //Xpath button "Se connecter"
-    WebElement buttonConnect = driver.findElement(By.xpath("//input[@id=\"button\"]"));
-    //Xpath button Logout
-    WebElement buttonLogout = driver.findElement(By.xpath("//a[@class=\"cerrar_sesion\"]"));
+    @FindBy(xpath = "//input[@id=\"textfield\"]")
+    WebElement userField;
 
-    public LoginLogout(WebDriver driver, WebElement userField) {
+    @FindBy(xpath = "//input[@id=\"textfield2\"]")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//input[@id=\"button\"]")
+    WebElement buttonConnect;
+
+    @FindBy(xpath = "//a[@class=\"cerrar_sesion\"]")
+    WebElement buttonLogout;
+
+    public LoginLogout(WebDriver driver) {
         super(driver);
     }
     //Method to login
@@ -33,5 +37,6 @@ public class LoginLogout extends ToolBox {
     public LoginLogout logout(){
         buttonLogout.click();
         return PageFactory.initElements(driver,LoginLogout.class);
+
     }
 }
