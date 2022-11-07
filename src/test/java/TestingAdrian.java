ad3b3aa313b60194d521852d69cb9d871c504b89
AdrianANDRE <andre.adrian11@gmail.com>
2022-11-07 20:11:46 +0100

    Update Git

--- src/test/java/TestingAdrian.java
+++ src/test/java/TestingAdrian.java
@@ -1,33 +1,24 @@
-import PageObject.Calendrier;
 import PageObject.Home;
 import PageObject.Login;
-import PageObject.ToolBox;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import static org.junit.Assert.assertTrue;
 
-public class TestingAdrian extends ToolBox {
+public class TestingAdrian extends AbstractTest {
 
-    public TestingAdrian() {
-        super(driver);
-    }
-
-    @Before
-    public void initialization () {
-        ToolBox.startup();
-    }
     @Test
-    public void connect () throws InterruptedException {
-        //à effectuer à chaque methode de test pour faire usage de la classe login
+    public void connect () {
+//        //à effectuer à chaque methode de test pour faire usage de la classe login
         Login log = PageFactory.initElements(driver, Login.class);
-        //"Classe de destination" Variable = log."methode"();
+//        //"Classe de destination" Variable = log."methode"();
         Home home = log.login();
-        Thread.sleep(2000);
-        Calendrier calendrier = home.accessCalendriers();
-        Thread.sleep(2000);
+//        home.logout();
+        home.moveToCalendrier();
+
+        public void cliquer(){
+            assertTrue(action.())
+        }
         driver.quit();
 
 
